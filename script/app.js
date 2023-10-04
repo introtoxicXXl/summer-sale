e8cee0826a7dd0482ce35fb93801b5e907f6c071
introtoxicXXl <minhajulabedin648@gmail.com>
2023-10-04 15:32:00 +0600

    coupon btn function added

--- script/app.js
+++ script/app.js
@@ -1,1 +1,19 @@
-console.log('connect')
+
+
+function coupon() {
+    const couponInput = document.getElementById('coupon-input');
+    const couponValue = couponInput.value;
+    document.getElementById('coupon-input').value = '';
+    const addPrice = parseFloat(document.getElementById('add-price').innerText);
+    console.log(addPrice)
+
+    if (couponValue === 'SELL200') {
+        const textDiscountPrice = document.getElementById('discount-price').innerText;
+        const discountPrice = (addPrice * 0.2).toFixed(2);
+        document.getElementById('discount-price').innerText = discountPrice;
+        document.getElementById('total-price').innerText = (addPrice - discountPrice).toFixed(2);
+    } else {
+        alert('Please Write Valid Coupon Code');
+    }
+
+}
