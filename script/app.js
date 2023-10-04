f3b68578221460fe3016dfbdcb2916d676809b36
introtoxicXXl <minhajulabedin648@gmail.com>
2023-10-04 15:33:08 +0600

    purchase btn function added

--- script/app.js
+++ script/app.js
@@ -1,4 +1,24 @@
+function addToCart(id, name) {
+    const textPrice = document.getElementById(id).innerText;
+    const textAddPrice = document.getElementById('add-price').innerText;
+    const productNameContainer = document.getElementById('product-name-container');
+    const price = parseFloat(textPrice);
+    const addPrice = parseFloat(textAddPrice);
+    const newPrice = (price + addPrice).toFixed(2);
+    document.getElementById('add-price').innerText = newPrice;
+    document.getElementById('total-price').innerText = newPrice;
 
+    const p = document.createElement('p');
+    p.innerHTML = `<p class="text-2xl font-medium">${productNameContainer.childElementCount + 1} ${name}</p>`;
+    productNameContainer.appendChild(p);
+
+    if (newPrice > 0) {
+        document.getElementById('purchase-btn').removeAttribute('disabled');
+    }
+    if (newPrice >= 200) {
+        document.getElementById('coupon-btn').removeAttribute('disabled');
+    }
+}
 
 function coupon() {
     const couponInput = document.getElementById('coupon-input');
